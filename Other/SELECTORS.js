10bcaabbdd0ef3b15b41e2e422510411d238832a
Carteahere <61406895+SadYuyuko@users.noreply.github.com>
2026-09-09 13:30:10 +0800

    Update SELECTORS.js

--- Other/SELECTORS.js
+++ Other/SELECTORS.js
@@ -1,35 +1,41 @@
   const SELECTORS = {
     bing: {
+      match: /(?:^|\.)bing\.(?:com|[a-z]{2}(?:\.[a-z]{2})?)$/,
       containers: 'li.b_algo, div.b_algo',
       titles: ['h2 a', 'a h2', '.b_title'],
       snippets: ['.b_caption p', '.b_snippet', '.b_paractl p', '.b_lineclamp2'],
       links: 'a[href]',
     },
     google: {
+      match: /(?:^|\.)google\.(?:[a-z]{2,3}(?:\.[a-z]{2})?|[a-z]{4,})$/,
       containers: 'div.g, div.MjjYud',
       titles: ['h3', 'div[role="heading"]', '.LC20lb', '.DKV0Md', '.sXLaOe', '.c9DxTc', 'a h3'],
       snippets: ['.st', '.VwiC3b', '.s3v9rd', '.IsZvec', '.lyLwlc', '.yXK7lf'],
       links: 'a[href]',
     },
     duckduckgo: {
+      match: /(?:^|\.)(?:duckduckgo\.com|ddg\.gg)$/,
       containers: '[data-testid="result"], .result, .web-result, .tile, .tile--ad',
       titles: ['a[data-testid="result-title-a"]', '.result__title', '.tile__title', '.tile--title__title', 'h2 a', 'a h2'],
       snippets: ['[data-testid="result-snippet"]', '[data-result="snippet"]', '.result__snippet'],
       links: ['a[data-testid="result-extras-url-link"]', 'a[data-testid="result-title-a"]', '.result__url', '.tile--title__domain', 'a[href]'],
     },
     yandex: {
+      match: /(?:^|\.)(?:ya\.ru|yandex\.(?:[a-z]{2,3}(?:\.[a-z]{2})?|[a-z]{4,}))$/,
       containers: 'div.Organic',
       titles: ['.OrganicTitle'],
       snippets: ['.OrganicText'],
       links: ['.OrganicTitle a', '.Path-Item a', 'a.Link', 'a[href]'],
     },
     brave: {
+      match: /(?:^|\.)brave\.com$/,
       containers: '.snippet[data-type="web"], .snippet[data-type="news"], .snippet[data-type="videos"], .image-wrapper',
       titles: ['.title', '.snippet-title', '.img-title'],
       snippets: ['.generic-snippet .content', '.generic-snippet', '.line-clamp-dynamic', '.snippet-description', '.description'],
       links: ['a[href]'],
     },
     yahoo: {
+      match: /(?:^|\.)yahoo\.(?:com|[a-z]{2}(?:\.[a-z]{2})?)$/,
       containers: '.sw-Card.Algo, li.b_algo, div.b_algo, #web .algo, .algo-sr, .richAlgo',
       titles: ['h3', '.s-title', 'h2 a', 'a h2', '.b_title', '.title'],
       snippets: ['.sw-Card__description', '.sw-Card__snippet', '.sw-Text__body', 'p', '.b_caption p', '.b_snippet', '.b_paractl p'],
